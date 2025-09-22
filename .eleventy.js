e234138f7b93279a12b97a5a99dc6f7eb1ef85e0
perrystudios <73252481+perrystudios@users.noreply.github.com>
2025-09-22 17:47:58 -0400

    config image wrap on images for articles

--- .eleventy.js
+++ .eleventy.js
@@ -9,14 +9,19 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addCollection("articles", function (collectionApi) {
     return collectionApi.getFilteredByGlob("article/*.md").sort((a, b) => b.date - a.date);
   });
-
+  // Enable manual image wrapping on article images
+  eleventyConfig.addPairedShortcode("imageWrap", function(content){
+    return `<figure class="styled-image">
+            ${content}
+            </figure>`;
+  });
   return {
     dir: {
       input: ".",       // 👈  root content folder
       includes: "_includes",
       output: "_site"
     },
-    markdownTemplateEngine: "njk",
+    markdownTemplateEngine: "njk", // enables nunjucks in markdown
     htmlTemplateEngine: "njk",
     dataTemplateEngine: "njk"
   };
