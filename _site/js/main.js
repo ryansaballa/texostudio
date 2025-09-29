d4e529b8245a6768b2768777af3dbd5c3b4a51c5
perrystudios <73252481+perrystudios@users.noreply.github.com>
2025-09-29 08:58:59 -0400

    toggle menu between hamburger and close icon

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,8 +1,11 @@
-document.addEventListener ('DOMContentLoaded', function () {
-    const toggle = document.querySelector('menu-toggle');
-    const menu = document.querySelector('nav');
+document.addEventListener('DOMContentLoaded', function(){
+    const toggle = document.querySelector('.menu-toggle');
+    const nav = document.querySelector('nav');
 
-    toggle.addEventListener('click', function (){
-        console.log("test")
+    toggle.addEventListener('click', function(){
+        toggle.classList.toggle('active'); // icon switch between hamburger and close-icon
+        nav.classList.toggle('open'); // showing and hiding nav menu
     })
+
 })
+
